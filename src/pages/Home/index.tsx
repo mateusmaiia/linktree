2c127fa3c8b21285df6481733217efd4014a979d
mateus maia <matteusmaia889@gmail.com>
2023-09-29 16:53:47 -0300

    Login props e UseState

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import { FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa'
 
 export function Home(){
     return(
-        <div className="flex flex-col w-full py-4 items-center justify-center">
+        <div className="flex flex-col w-full h-screen py-4 items-center justify-center">
             <h1 className="md:text-4xl text-3xl font-bold text-white mt-20 ">Mateus Maia</h1>
             <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
 
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,37 @@
 import { Link } from "react-router-dom";
 import { Input } from "../../components/input";
+import { useState, FormEvent } from "react";
 
 export function Login(){
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handleSubmit(event:FormEvent){
+        event.preventDefault()
+    }
+
     return(
-        <div className="flex w-full h-screan items-center justify-center flex-col">
+        <div className="flex w-full h-screen items-center justify-center flex-col">
             <Link to="/">
                 <h1 className="mt-11 text-white mb-7 font-bold text-5xl ">Dev
                     <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">Link</span>
                 </h1>
             </Link>
-            <form className="w-full max-w-xl flex flex-col px-2"> {/*px-2 espaçamento pra nao ficar grudado*/}
-                <Input />
+            <form onSubmit={handleSubmit} className="w-full max-w-xl flex flex-col px-2"> {/*px-2 espaçamento pra nao ficar grudado*/}
+                <Input 
+                    type="gmail"
+                    placeholder="Digite seu email..."
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input 
+                    type="password"
+                    placeholder="**********"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
 
-                <button type="submit" className="h-9 bb-blue rounded border-0 text-lg font-medium text-white">
+                <button type="submit" className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white">
                     Acessar
                 </button>
 
