6760395ff3b7d6fef28bca98742dd6b0b8fce36b
mateus maia <matteusmaia889@gmail.com>
2023-09-29 17:23:57 -0300

    Update index.tsx

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 export function NotFound(){
     return(
         <div className='h-screen w-full flex items-center justify-center flex-col'>
-            <h1 className='mb-2 text-5xl '>Not found 404</h1>
+            <h1 className='mb-4 text-5xl text-white'>Not found 404</h1>
             <p className='bg-slate-500 rounded-xl px-4 py-1 text-white text-2xl hover:cursor-pointer hover:scale-110'><Link to={'/'}>Página Home</Link></p>
         </div>
     )
