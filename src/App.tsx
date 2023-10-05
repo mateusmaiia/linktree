098585767319b98a75d82db8757868b5a747a036
mateus maia <matteusmaia889@gmail.com>
2023-10-05 15:19:31 -0300

    Private Protegendo rota de admin

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import {Home} from './pages/Home'
 import {Admin} from './pages/Admin'
 import {Login} from './pages/Login'
 import {NotFound} from './pages/NotFound'
+import { Private } from "./router/Private"
+import { Networks } from "./pages/Networks"
 
 export const router = createBrowserRouter([
   {
@@ -12,11 +14,15 @@ export const router = createBrowserRouter([
   },
   {
     path: '/admin',
-    element: <Admin/>
+    element: <Private><Admin/></Private> //vai passar primeiro pelo private, se o private permitir ele vai renderizar o children
   },
   {
     path: '/login',
     element: <Login/>
+  },
+  {
+    path:"/admin/social",
+    element: <Private><Networks /></Private> //Private pode ficar ao redor de qualquer Página agora.  PROTEÇÃO DE ROTA
   },
   {
     path: '*',
